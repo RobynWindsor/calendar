897d699382012c7c3346f11f87825fa177632a44
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-09-18 15:24:35 +0100

    Added dates in JS

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
-
+const datetxtElement = document.querySelector('.datetxt');
+const datesElement = document.querySelector('.dates');
+const btnElement = document.querySelectorAll('.calendar_headings .fa-solid');
+const monthYearElement = document.querySelector('.month_year');
 let dmObj = {
   days: [
     'Sunday',
@@ -26,3 +29,14 @@ let dmObj = {
     'December',
   ],
 };
+
+let dateObj = new Date();
+
+let dayName = dmObj.days[dateObj.getDay()];
+let month = dateObj.getMonth();
+let year = dateObj.getFullYear();
+let date = dateObj.getDate();
+
+console.log(dayName, month, year, date);
+
+datetxtElement.innerHTML = `${dayName}, ${date}, ${dmObj.months[month]}, ${year}`;
