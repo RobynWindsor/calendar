afc471738aabf0cf14054efeb906adf113635642
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-09-18 15:15:30 +0100

    add days and month object

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let dmObj = {
+  days: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ],
+
+  months: [
+    'Januray',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+};
